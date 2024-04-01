c464ca78fb3b78e8e1aff4e3393227bc13e1b45c
Baptiste <73223380+Baptiste-Mrmt@users.noreply.github.com>
2024-04-01 16:07:02 +0200

    🚧 Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+	return (
+		<div>
+			<h1>Home</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+				animi repellendus quidem optio debitis, cumque temporibus doloribus eos
+				ad ipsa necessitatibus harum sed. Architecto at in cupiditate. Labore,
+				necessitatibus quam!
+			</p>
+		</div>
+	);
+};
