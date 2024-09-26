0ae6f07bc9c7f09e0e7cf6bc78ca9a6ac6fcbe84
Baptiste <b.miramont@groupe-adonis.fr>
2024-09-26 19:06:06 +0200

    ✨ New driver type

--- src/types/driver.ts
+++ src/types/driver.ts
@@ -9,3 +9,22 @@ export type DriverType = {
 	grandPrixParticipation: number;
 	team: string;
 };
+
+export type DriverDetailedType = {
+	id: number;
+	firstName: string;
+	lastName: string;
+	avatar: string;
+	country: string;
+	worldChampionshipsTitle: number;
+	podiums: number;
+	grandPrixParticipation: number;
+	teams: [
+		{
+			id: number;
+			name: string;
+			favicon: string;
+			isCurrentTeam: boolean;
+		},
+	];
+};
