dc26db23e052de8eb4b6cfba3fd29e0c806960c3
Baptiste <b.miramont@groupe-adonis.fr>
2024-10-27 14:48:33 +0100

    ✨ PWA configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import * as path from "path";
 
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [
+		react(),
+		TanStackRouterVite(),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
+			manifest: {
+				name: "Formulix",
+				short_name: "Formulix",
+				description:
+					"Formulix: Dive into F1 history and present ! Explore detailed profiles of teams and drivers from both the current season and past years, all in one app.",
+				theme_color: "#f5f5f5",
+			},
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,ico,png,ttf}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
+		}),
+	],
 	server: {
 		host: true,
 	},
