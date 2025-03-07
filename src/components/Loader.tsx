6c479b48c54f760304f147d430380f29bd187d72
baptistemiramont <baptiste@baptistemiramont.fr>
2025-03-07 23:14:57 +0100

    ✨ Enhance Loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,26 @@
+import { css } from "@/../styled-system/css";
+
 export const Loader = () => {
+	const loaderStyles = {
+		container: {
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			position: "relative",
+			overflow: "hidden",
+			width: "100%",
+		},
+		wheel: {
+			padding: 8,
+			fontSize: "2rem",
+			animation: "spin 1.5s linear infinite",
+			position: "relative",
+		},
+	};
+
 	return (
-		<div>
-			<p>🏎️🏎️🏎️</p>
+		<div className={css(loaderStyles.container)}>
+			<p className={css(loaderStyles.wheel)}>🛞</p>
 		</div>
 	);
 };
